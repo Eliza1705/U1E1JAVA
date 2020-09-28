671670538551b5e7701eeb49bd16fee05634f9cf
Eliza1705 <71892559+Eliza1705@users.noreply.github.com>
2020-09-28 13:29:40 -0500

    Create OrdenarUnaFrase.java

---
+++ OrdenarUnaFrase.java
@@ -0,0 +1,14 @@
+package ordenar.una.frase;
+
+/**
+ *
+ * @author A Eliza Amaro
+ */
+public class OrdenarUnaFrase {
+
+    public static void main(String[] args) {
+        Procesos m = new Procesos();
+		m.procesos();
+    }
+    
+}
